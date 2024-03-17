d394e082cd49e398730bf565625f4d5e5dc78e4b
navintc <navin.thamindu@gmail.com>
2024-03-17 21:12:09 +0530

    Initial Commit

---
+++ ClothingStore-iOS/ClothingStore_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingStore_iOSApp.swift
+//  ClothingStore-iOS
+//
+//  Created by Navin Thamindu on 2024-03-17.
+//
+
+import SwiftUI
+
+@main
+struct ClothingStore_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClothingStore-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClothingStore-iOS
+//
+//  Created by Navin Thamindu on 2024-03-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
