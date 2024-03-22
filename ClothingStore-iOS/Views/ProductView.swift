b2a16fc08ef270782eca0614c33d7f31e85b47ec
navintc <navin.thamindu@gmail.com>
2024-03-23 02:30:32 +0530

    Adding design updates and home view data intergrations

---
+++ ClothingStore-iOS/Views/ProductView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductView.swift
+//  ClothingStore-iOS
+//
+//  Created by Navin Thamindu on 2024-03-22.
+//
+
+import SwiftUI
+
+struct ProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductView()
+}
