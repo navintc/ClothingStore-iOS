f5d32350be5bc7a310f893fb5fb92964bb16ee10
navintc <navin.thamindu@gmail.com>
2024-03-30 00:44:35 +0530

    Fixing cart/payment face ID authentication

---
+++ ClothingStore-iOS/Views/PaymentSuccessView.swift
@@ -0,0 +1,47 @@
+//
+//  PaymentSuccess.swift
+//  ClothingStore-iOS
+//
+//  Created by Navin Thamindu on 2024-03-29.
+//
+
+import SwiftUI
+
+struct PaymentSuccess: View {
+    var body: some View {
+        VStack{
+            Spacer()
+            VStack{
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(BrandPrimary)
+                    .symbolEffect(.variableColor.iterative, options: .repeating, value: true)
+
+                
+                Text("Payment Success").font(.title)
+            }
+            
+            Spacer()
+            Button(action: {
+                // Action for checkout button
+               
+            }) {
+                Text("Continue")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(BrandPrimary)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+        }
+        .padding()
+        .navigationTitle("Payment")
+    }
+}
+
+#Preview {
+    PaymentSuccess()
+}
